f32508fc55bf5b8b8f5ce2d085b618132e86a1aa
yuu-eguci <yuu.eguci@gmail.com>
2025-04-06 19:00:40 +0900

    💄 createTheme を使ってみたい - main.tsx

--- webapp/frontend-react/src/main.tsx
+++ webapp/frontend-react/src/main.tsx
@@ -1,14 +1,37 @@
-import App from '@/App'
-import '@/i18n'
-import '@/index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import App from "@/App";
+import "@/i18n";
+import "@/index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fdd835",
+    },
+    secondary: {
+      main: "#8d6e63",
+    },
+    error: {
+      main: "#e53935",
+    },
+    info: {
+      main: "#1e88e5",
+    },
+    success: {
+      main: "#43a047",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
