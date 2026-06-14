f86ef8b7f16278a3d15c60359abfdf7e67b4bbfe
Yuu Eguci <yuu.eguci@gmail.com>
2026-06-14 20:46:05 +0900

    🔥 V1 (HomePage) 削除 (#36)

--- webapp/frontend-react/src/App.tsx
+++ webapp/frontend-react/src/App.tsx
@@ -1,5 +1,4 @@
 import "@/App.css"
-import HomePage from "@/pages/HomePage"
 import HomePageV2 from "@/pages/HomePageV2"
 import LorePage from "@/pages/LorePage"
 import NotFoundPage from "@/pages/NotFoundPage"
@@ -9,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePageV2 />} />
-      <Route path="/v1" element={<HomePage />} />
       <Route path="/lore" element={<LorePage />} />
       {/* その他のパスは 404 */}
       <Route path="*" element={<NotFoundPage />} />
